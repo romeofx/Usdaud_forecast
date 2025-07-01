b8d41df683bbf4738158a407efbe16f60bd23ac1
Calistus Ndubuisi <calistusndubuisi4@gmail.com>
2025-07-01 16:28:59 +0100

    Initial Full project update

--- replit.nix
+++ replit.nix
@@ -4,6 +4,12 @@
     pkgs.python311Packages.pip
     pkgs.python311Packages.setuptools
     pkgs.python311Packages.wheel
+    pkgs.python311Packages.numpy
+    pkgs.python311Packages.scikit-learn
+    pkgs.python311Packages.joblib
+    pkgs.python311Packages.xgboost
+    pkgs.python311Packages.fastapi
     pkgs.python311Packages.uvicorn
+    pkgs.python311Packages.jinja2
   ];
 }
