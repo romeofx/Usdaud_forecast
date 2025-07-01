0a5321a3ea853abbe0fe28804ac5012e043fbcf0
Calistus Ndubuisi <calistusndubuisi4@gmail.com>
2025-07-01 16:18:42 +0100

    replit.nix update

---
+++ replit.nix
@@ -0,0 +1,9 @@
+{ pkgs }: {
+  deps = [
+    pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.python311Packages.setuptools
+    pkgs.python311Packages.wheel
+    pkgs.python311Packages.uvicorn
+  ];
+}
